70b02594dc23227f50ce00c24679a4f3ffa297a7
Olivier <olivier.gonson@gmail.com>
2018-01-31 22:10:44 -0500

    enable screen in main script

--- smart_alarm_clock.py
+++ smart_alarm_clock.py
@@ -2,14 +2,16 @@
 import time
 import json
 from mopidy import Mopidy
+from scrollphathd import ScrollpHatHD
 
 def main():
 
     # Create objects
     mopidy = Mopidy()
+    screen = ScrollpHatHD()
 
     while True:
-
+        screen.show()
 
         time.sleep(0.1)
 
